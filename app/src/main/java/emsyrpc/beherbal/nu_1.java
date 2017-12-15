fde2fb2b905a4fb0e60d4ff33dac4ecd4c95e8d1
BeHerbal <programasdeenzo@hotmail.com>
2017-12-15 13:51:43 -0300

    conexion base de datos activity n1

--- app/src/main/java/emsyrpc/beherbal/nu_1.java
+++ app/src/main/java/emsyrpc/beherbal/nu_1.java
@@ -77,7 +77,7 @@ public class nu_1 extends AppCompatActivity {
 
 
         //conexion a base de datos
-       conn=new BaseDeDatos(getApplicationContext());
+      // conn=new BaseDeDatos(getApplicationContext());
 
         //spinner Edad
 
@@ -112,10 +112,10 @@ public class nu_1 extends AppCompatActivity {
 
                 Intent intent= new Intent(nu_1.this, nu_2.class);
                 startActivity(intent);
-               // selectSexo=sexo.getSelectedItem().toString();
-               // selectEdad= Integer.parseInt(edad.getSelectedItem().toString());
-               // nameU=nombre.getText().toString();
-               // apellU=apellido.getText().toString();
+                selectSexo=sexo.getSelectedItem().toString();
+                selectEdad= Integer.parseInt(edad.getSelectedItem().toString());
+                nameU=nombre.getText().toString();
+                apellU=apellido.getText().toString();
 
             }
         });
@@ -157,7 +157,7 @@ public class nu_1 extends AppCompatActivity {
     //metodo Cunsultar sexo de base de datos
     private void ConsultarListaSexo() {
 
-        conn=new BaseDeDatos(getApplicationContext());
+        conn=new BaseDeDatos(this.getApplicationContext());
         SQLiteDatabase db=conn.getReadableDatabase();
 
         sexoU sx=null;
@@ -192,7 +192,7 @@ public class nu_1 extends AppCompatActivity {
 
     //metodo Cunsultar edad de base de datos
     private void ConsultarListaEdad() {
-        conn=new BaseDeDatos(getApplicationContext());
+        conn=new BaseDeDatos(this.getApplicationContext());
         SQLiteDatabase db=conn.getReadableDatabase();
 
         EdadU ed=null;
